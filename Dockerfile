c9eac7d6b528025a303767f8df9fbd5ee377ae0d
twogg-git <cruz.catherin@gmail.com>
2017-08-01 00:36:41 -0500

    Set to review
    added randon over array to stat.Mean

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM alpine:latest
-
-WORKDIR "/opt"
-
-ADD .docker_build/go-heroku-math /opt/bin/go-heroku-math
-
-CMD
--- main.go
+++ main.go
@@ -2,53 +2,29 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"os"
-  	"time"
 	"math/rand"
-  	"github.com/gonum/stat"
+
+	"github.com/gonum/stat"
 )
 
 func main() {
-	http.HandleFunc("/", mathTest)
-	fmt.Println("listening...")
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-	if err != nil {
-		panic(err)
+
+	// I dont want to do this
+	//dataPool := []float64{rand.Float64(), rand.Float64(), rand.Float64(), rand.Float64()}
+
+	// Instead i want to use rand.Float64() to fill the DataPool, like 100 Float64 numbers
+
+	var dataPool [100]float64
+	for i := 0; i < 100; i++ {
+		dataPool[i] = rand.Float64()
 	}
-}
 
-func mathTest(res http.ResponseWriter, req *http.Request) {
-	
-  	fmt.Fprintln(res, "Hello, datapool testing")
-  
-  	now := time.Now()
-  	fmt.Fprintln(res, now)
-  	
-  	const poolSize int = 10000
-  	const randLimit int = 2500
-  	var dataPool2 [poolSize]int
-  
-  	fmt.Fprintln(res, "Adding data into the pool")
-  	for i:= 0; i < poolSize; i++ {
-      	dataPool2[i] = rand.Intn(randLimit)
-  	}
-  	
-  	fmt.Fprintln(res, "Pool size")
-  	fmt.Fprintln(res, poolSize)
-    fmt.Fprintln(res, "Random number limit")             
-  	fmt.Fprintln(res, randLimit)
+	// stat.Mean only calculates over []float64 arrays
+	mean := stat.Mean(dataPool, nil)
+	fmt.Println("Mean value", mean)
 
-  	fmt.Fprintln(res, "Datapool size", len(dataPool2))
-  	
-  	media := stat.Mean(dataPool2)	
-  	fmt.Fprintln(res, "Median")
-  	fmt.Fprintln(res, media)
-  
-  	for i:=0; i < len(dataPool2); i++ {
-      	fmt.Fprintln(res, dataPool2[i])
-  	} 
-  
- 	fmt.Fprintln(res, "Yes it's alive!!!")
-
+	fmt.Println("DataPool...")
+	for i := 0; i < len(dataPool); i++ {
+		fmt.Println(dataPool[i])
+	}
 }
