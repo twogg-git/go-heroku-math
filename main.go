451c1aa73ee8c2093c9e08d74f5a8729b1ede3fb
Catherin Cruz <cruz.catherin@gmail.com>
2017-08-23 10:50:29 -0500

    Update main.go

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-
+	//"math/rand"
 	"net/http"
 
-	"github.com/gonum/stat"
+	//"github.com/gonum/stat"
 )
 
 func main() {
@@ -21,24 +20,24 @@ func main() {
 func statTest(res http.ResponseWriter, req *http.Request) {
 
 	fmt.Fprintln(res, "Testing , gonum/stat fuctions")
+	/*
+		poolSize := 100000
+		fmt.Fprintln(res, "Pool size", poolSize)
 
-	poolSize := 100000
-	fmt.Fprintln(res, "Pool size", poolSize)
+		var dataPool = make([]float64, poolSize)
+		for i := 0; i < poolSize; i++ {
+			dataPool[i] = rand.Float64()
+		}
 
-	var dataPool = make([]float64, poolSize)
-	for i := 0; i < poolSize; i++ {
-		dataPool[i] = rand.Float64()
-	}
+		// stat.Mean only calculates over []float64 arrays
+		mean := stat.Mean(dataPool, nil)
+		fmt.Fprintln(res, "Mean value", mean)
 
-	// stat.Mean only calculates over []float64 arrays
-	mean := stat.Mean(dataPool, nil)
-	fmt.Fprintln(res, "Mean value", mean)
+		stddev := stat.StdDev(dataPool, nil)
+		fmt.Fprintln(res, "Standar deviation", stddev)
 
-	stddev := stat.StdDev(dataPool, nil)
-	fmt.Fprintln(res, "Standar deviation", stddev)
-
-	/*fmt.Println("DataPool...")
-	for i := 0; i < len(dataPool); i++ {
-		fmt.Println(dataPool[i])
-	}*/
+		/*fmt.Println("DataPool...")
+		for i := 0; i < len(dataPool); i++ {
+			fmt.Println(dataPool[i])
+		}*/
 }
