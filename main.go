385889fad744e7343dc1f0c4abcb95ae4cd1e953
Catherin Cruz <cruz.catherin@gmail.com>
2017-08-23 10:35:07 -0500

    Update main.go

--- main.go
+++ main.go
@@ -4,22 +4,38 @@ import (
 	"fmt"
 	"math/rand"
 
+	"net/http"
+
 	"github.com/gonum/stat"
 )
 
 func main() {
+	http.HandleFunc("/", statTest)
+	fmt.Println("listening...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
 
-	poolSize := 10000
+func statTest(res http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintln(res, "Testing , gonum/stat fuctions")
+
+	poolSize := 100000
+	fmt.Fprintln(res, "Pool size", poolSize)
+
 	var dataPool = make([]float64, poolSize)
 	for i := 0; i < poolSize; i++ {
 		dataPool[i] = rand.Float64()
 	}
-	
+
+	// stat.Mean only calculates over []float64 arrays
 	mean := stat.Mean(dataPool, nil)
-	fmt.Println("Mean value", mean)
+	fmt.Fprintln(res, "Mean value", mean)
 
 	stddev := stat.StdDev(dataPool, nil)
-	fmt.Println("Standar deviation", stddev)
+	fmt.Fprintln(res, "Standar deviation", stddev)
 
 	/*fmt.Println("DataPool...")
 	for i := 0; i < len(dataPool); i++ {
